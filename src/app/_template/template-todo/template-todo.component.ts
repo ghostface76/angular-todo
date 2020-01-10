ddfa800b50089968284318127b4e882821a8fc08
jens-home <ghostface76@gmail.com>
2020-01-10 22:37:39 +0100

    erster upload

---
+++ src/app/_template/template-todo/template-todo.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ToDo } from '../../_interface/todo';
+
+@Component({
+  selector: 'app-template-todo',
+  templateUrl: './template-todo.component.html',
+  styleUrls: ['./template-todo.component.sass']
+})
+export class TemplateTodoComponent implements OnInit {
+
+    public toDo$: ToDo;
+
+    constructor() {
+      this.toDo$ = {
+        id: 1,
+        label: 'Wie gehts',
+        status: false,
+        position: 1
+      };
+     }
+
+    ngOnInit() {
+    }
+
+    public changeCheck(event?: any): void {
+      this.toDo$.status = !this.toDo$.status;
+    }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TemplateTodoComponent } from './_template/template-todo/template-todo.component';
+import { PageListComponent } from './page-list/page-list.component';
+import { TemplateTodoFormComponent } from './_template/template-todo-form/template-todo-form.component';
+import { TemplateHeaderComponent } from './_template/template-header/template-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateTodoComponent,
+    PageListComponent,
+    TemplateTodoFormComponent,
+    TemplateHeaderComponent
   ],
   imports: [
     BrowserModule
